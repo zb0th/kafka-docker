238d875948df857febe48c9822e9664eb9a20b76
Guevara, Alexei <alguevara@ebay.com>
2014-09-15 22:55:54 -0400

    added JMX and graphite integration

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,16 @@
-FROM ubuntu
+FROM williamyeh/java7
 
-MAINTAINER Wurstmeister 
-RUN echo "deb http://archive.ubuntu.com/ubuntu precise universe" >> /etc/apt/sources.list; apt-get update; apt-get install -y unzip  openjdk-6-jdk wget git
+MAINTAINER Wurstmeister
+RUN echo "deb http://archive.ubuntu.com/ubuntu precise universe" >> /etc/apt/sources.list; apt-get update; apt-get install -y unzip wget git
 
-RUN wget -q http://mirror.gopotato.co.uk/apache/kafka/0.8.1.1/kafka_2.8.0-0.8.1.1.tgz -O /tmp/kafka_2.8.0-0.8.1.1.tgz
-RUN tar xfz /tmp/kafka_2.8.0-0.8.1.1.tgz -C /opt
+RUN wget -q http://mirror.its.dal.ca/apache/kafka/0.8.1.1/kafka_2.10-0.8.1.1.tgz -O /tmp/kafka_2.10-0.8.1.1.tgz
+RUN tar xfz /tmp/kafka_2.10-0.8.1.1.tgz -C /opt
 
-ENV KAFKA_HOME /opt/kafka_2.8.0-0.8.1.1
-ADD start-kafka.sh /usr/bin/start-kafka.sh 
-CMD start-kafka.sh 
+ENV KAFKA_HOME /opt/kafka_2.10-0.8.1.1
+ADD kafka-graphite-1.0.0.jar /opt/kafka_2.10-0.8.1.1/libs/
+ADD metrics-graphite-2.2.0.jar /opt/kafka_2.10-0.8.1.1/libs/
+ADD metrics-core-2.2.0.jar /opt/kafka_2.10-0.8.1.1/libs/
+ADD slf4j-simple-1.7.2.jar /opt/kafka_2.10-0.8.1.1/libs/
+
+ADD start-kafka.sh /usr/bin/start-kafka.sh
+CMD start-kafka.sh
--- start-kafka.sh
+++ start-kafka.sh
@@ -7,4 +7,29 @@ if [ "$KAFKA_HEAP_OPTS" != "" ]; then
     sed -r -i "s/^(export KAFKA_HEAP_OPTS)=\"(.*)\"/\1=\"$KAFKA_HEAP_OPTS\"/g" $KAFKA_HOME/bin/kafka-server-start.sh
 fi
 
+#
+# merge in graphite reporter properties if necessary
+#
+if [ "$GRAPHITE_ENABLED" = "True" ]; then
+    echo "############################# Graphite #############################" >> $KAFKA_HOME/config/server.properties
+    echo "kafka.metrics.reporters=com.criteo.kafka.KafkaGraphiteMetricsReporter" >> $KAFKA_HOME/config/server.properties
+    echo "kafka.graphite.metrics.reporter.enabled=true" >> $KAFKA_HOME/config/server.properties
+
+    echo "kafka.metrics.polling.interval.secs=5" >> $KAFKA_HOME/config/server.properties
+
+    echo "kafka.graphite.metrics.host=$GRAPHITE_1_PORT_2003_TCP_ADDR" >> $KAFKA_HOME/config/server.properties
+    echo "kafka.graphite.metrics.port=$GRAPHITE_1_PORT_2003_TCP_PORT" >> $KAFKA_HOME/config/server.properties
+
+    if [ "$GRAPHITE_GROUP" != "" ]; then
+      echo "kafka.graphite.metrics.group=$GRAPHITE_GROUP" >> $KAFKA_HOME/config/server.properties
+    fi
+    if [ "$GRAPHITE_METRICS_EXCLUDE_REGEX" != "" ]; then
+      echo "kafka.graphite.metrics.exclude.regex=$GRAPHITE_METRICS_EXCLUDE_REGEX" >> $KAFKA_HOME/config/server.properties
+    fi
+fi
+
+echo ----
+cat $KAFKA_HOME/config/server.properties
+echo ----
+
 $KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
